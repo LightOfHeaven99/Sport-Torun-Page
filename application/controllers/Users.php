8c3c0980cf137fb81970e56131cf0a65817d3fdf
Kacper Burzała <kacperburzala@gmail.com>
2019-02-16 20:27:23 +0100

    private method to public

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -15,7 +15,27 @@ class Users extends CI_Controller {
       'callback_containsOnlyLetters' => 'Pole zawiera znaki inne niż litery.'
     ));
 
-    // TODO: Pozostałe reguły
+    $this->form_validation->set_rules('last-name', 'Nazwisko', 'required|callback_containsOnlyLetters', array(
+      'required' => 'Pole jest wymagane.',
+      'callback_containsOnlyLetters' => 'Pole zawiera znaki inne niż litery.'
+    ));
+
+    $this->form_validation->set_rules('uid', 'Login', 'required|callback_containsOnlyLettersAndNumbers', array(
+      'required' => 'Pole jest wymagane.',
+      'callback_containsOnlyLettersAndNumbers' => 'Pole zawiera znaki inne niż litery i cyfry.'
+    ));
+
+    $this->form_validation->set_rules('pwd', 'Hasło', 'required', array(
+      'required' => 'Pole jest wymagane.'
+    ));
+
+    $this->form_validation->set_rules('pwd-repeat', 'Hasło-powtórz', 'required', array(
+      'required' => 'Pole jest wymagane.'
+    ));
+
+    $this->form_validation->set_rules('mail', 'E-mail', 'required', array(
+      'required' => 'Pole jest wymagane.'
+    ));
 
     if($this->form_validation->run() === true) {
       // TODO: Obsługa rejestracji
@@ -28,6 +48,10 @@ class Users extends CI_Controller {
 
   public function containsOnlyLetters($string) {
     return preg_match("/^[a-ząćęłńóśźżA-ZĄĆĘŁŃÓŚŹŻ]*$/", $string);
+  }
+
+  public function containsOnlyLettersAndNumbers($string) {
+    return preg_match("/^[a-zA-Z0-9]*$/", $string);
   }
 
 }
