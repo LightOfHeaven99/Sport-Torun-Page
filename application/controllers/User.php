dd73ebc3945a7b7f24aac186e159dce5ece0593f
Kacper Burzała <kacperburzala@gmail.com>
2019-02-19 15:31:28 +0100

    próba1

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -52,7 +52,7 @@ class User extends CI_Controller
       redirect('/');
     }
 
-    $this->load->model('user_model');
+    
     $this->load->library('form_validation');
 
     $this->form_validation->set_rules('first-name', 'Imię', 'required|alpha');
