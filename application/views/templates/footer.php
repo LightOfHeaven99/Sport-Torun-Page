dada4abf7bbce6dbfae0d328f7f2f934d4d56b03
Główcześ <glowczes@gmail.com>
2019-02-23 21:15:45 +0100

    bez ikonek x2

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -35,7 +35,7 @@
 					</ul>
 				</div>
 			</div>
-				</hr>
+			<div class="row">
 			</div>
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
