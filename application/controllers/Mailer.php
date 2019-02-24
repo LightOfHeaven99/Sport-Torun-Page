c9ce1dcd61e10e1b65bd9e155c188e56a71aa07f
Główcześ <glowczes@gmail.com>
2019-02-24 16:16:12 +0100

    11

--- application/controllers/Mailer.php
+++ application/controllers/Mailer.php
@@ -12,20 +12,20 @@ class Mailer extends CI_Controller {
 
   public function sendmail()
   {
-      // $this->email->clear();
-      //
-      // $this->email->from('kacburza@interia.pl', 'Kacper Burzała');
-      // $this->email->to('biuro@tls-torun.pl');
-      // //$this->email->cc('another@another-example.com');
-      // //$this->email->bcc('them@their-example.com');
-      //
-      // $this->email->subject('Email Test');
-      // $this->email->message('Testowanie klasy email.');
-      //
-      // $this->email->send();
-      //
-      // echo $this->email->print_debugger();
-      // echo "Dziękujemy za wiadomość!";
+      $this->email->clear();
+
+      $this->email->from('kacburza@interia.pl', 'Kacper Burzała');
+      $this->email->to('biuro@tls-torun.pl');
+      //$this->email->cc('another@another-example.com');
+      //$this->email->bcc('them@their-example.com');
+
+      $this->email->subject('Email Test');
+      $this->email->message('Testowanie klasy email.');
+
+      $this->email->send();
+
+      echo $this->email->print_debugger();
+      echo "Dziękujemy za wiadomość!";
 
 
 
@@ -53,51 +53,50 @@ class Mailer extends CI_Controller {
 
 
 
+              // function IsInjected($str)
+              // {
+              //   $injections = array('(\n+)',
+              //               '(\r+)',
+              //               '(\t+)',
+              //               '(%0A+)',
+              //               '(%0D+)',
+              //               '(%08+)',
+              //               '(%09+)'
+              //               );
+              //   $inject = join('|', $injections);
+              //   $inject = "/$inject/i";
+              //   if(preg_match($inject,$str)){
+              //     return true;
+              //   }
+              //   else {
+              //     return false;
+              //   }
+              // }
+
       // $name = $_POST['name'];
       // $visitor_email = $_POST['email'];
       // $message = $_POST['message'];
 
-      $name = 'Kacper Burzała';
-      $visitor_email = 'kacburza@interia.pl';
-      $message = 'Siemka, jak się zapisać?';
+      // $name = 'Kacper Burzała';
+      // $visitor_email = 'kacburza@interia.pl';
+      // $message = 'Siemka, jak się zapisać?';
 
-      // if(IsInjected($visitor_email))
-      // {
-      //     echo "Bad email value!";
-      //     exit;
-      // }
+              // if(IsInjected($visitor_email))
+              // {
+              //     echo "Bad email value!";
+              //     exit;
+              // }
 
-      $email_from = 'no-reply@tls-torun.pl';
-      $email_subject = "ZAPISY";
-      $email_body = "You have received a new message from the user $name.\n".
-          "Here is the message:\n $message".
-
-      $to = "biuro@tls-torun.pl";
-      $headers = "From: $email_from \r\n";
-      $headers .= "Reply-To: $visitor_email \r\n";
-
-      mail($to,$email_subject,$email_body,$headers);
-
-
-      // function IsInjected($str)
-      // {
-      //   $injections = array('(\n+)',
-      //               '(\r+)',
-      //               '(\t+)',
-      //               '(%0A+)',
-      //               '(%0D+)',
-      //               '(%08+)',
-      //               '(%09+)'
-      //               );
-      //   $inject = join('|', $injections);
-      //   $inject = "/$inject/i";
-      //   if(preg_match($inject,$str)){
-      //     return true;
-      //   }
-      //   else {
-      //     return false;
-      //   }
-      // }
+      // $email_from = 'no-reply@tls-torun.pl';
+      // $email_subject = "ZAPISY";
+      // $email_body = "You have received a new message from the user $name.\n".
+      //     "Here is the message:\n $message".
+      //
+      // $to = "biuro@tls-torun.pl";
+      // $headers = "From: $email_from \r\n";
+      // $headers .= "Reply-To: $visitor_email \r\n";
+      //
+      // mail($to,$email_subject,$email_body,$headers);
   }
 
 
