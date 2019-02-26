0ec115ada6459cbbd864e4428d45d4e5606e68e4
Kacper Burzała <kacperburzala@gmail.com>
2019-02-26 23:24:36 +0100

    23

--- application/controllers/Mailer.php
+++ application/controllers/Mailer.php
@@ -6193,16 +6193,3 @@ class OAuth
         );
     }
 }
-
-class Exception extends \Exception
-{
-    /**
-     * Prettify error message output.
-     *
-     * @return string
-     */
-    public function errorMessage()
-    {
-        return '<strong>' . htmlspecialchars($this->getMessage()) . "</strong><br />\n";
-    }
-}
