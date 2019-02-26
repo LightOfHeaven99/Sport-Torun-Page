c9c30e15bbfbb5e735d65cd370bc4305022aa0fb
Główcześ <glowczes@gmail.com>
2019-02-26 19:59:43 +0100

    podmiana start statystki

--- application/views/pages/start.php
+++ application/views/pages/start.php
@@ -1,69 +1,112 @@
-  <!-- Page Content -->
-  <div class="start-bg">
-  <div class="container">
-    <div style="height:50px;"></div>
-    <div class="col-12 col-md-7 col-sm-12 hover-header d-none d-lg-block">
-      <div class="parallax-wrapper">
-       <div class="parallax-card">
-         <div class="card__cometOuter">
-         </div>
-         <div style="height: 20px;"></div>
-         <div class="row justify-content-center">
-           <div class="col">
-             <h4 class="header">Najbliższy mecz</h4>
-           </div>
-         </div>
-         <div class="row justify-content-center">
-           <div class="col">
-             <h5 class="header">odbędzie się za:</h5>
-           </div>
-         </div>
-         <div class="row justify-content-center">
-           <div id="clockdiv" class="clockdiv card__circle">
-             <div style="background-color: #333EE1;">
-               <span class="days"></span>
-               <div class="smalltext">Dni</div>
-             </div>
-             <div style="background-color: #E1D633;">
-               <span class="hours"></span>
-               <div class="smalltext">Godzin</div>
-             </div>
-             <div style="background-color: #333EE1;">
-               <span class="minutes"></span>
-               <div class="smalltext">Minut</div>
-             </div>
-             <div style="background-color: #E1D633;">
-               <span class="seconds"></span>
-               <div class="smalltext">Sekund</div>
-             </div>
-           </div>
-         </div>
-         <div style="height: 20px;"></div>
-         <div class="row justify-content-center">
-            <div class="row" style="padding: 2rem;">
-              <div class="col-sm-4"><h6 class="header">Toruńskie Pierniki</h6></div>
-              <div class="col-sm-4"><h6 class="header">:</h4></div>
-              <div class="col-sm-4"><h6 class="header">Wrocławska Zgroza</h6></div>
-            </div>
-         </div>
-        </div>
+<!-- Page Content -->
+<br>
+<div class="row">
+  <div class="col-2">col</div>
+
+  <div class="col-5">
+<!--Carousel Wrapper-->
+<div id="carousel-example-2" class="carousel slide carousel-fade" data-ride="carousel">
+  <!--Indicators-->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-2" data-slide-to="1"></li>
+    <li data-target="#carousel-example-2" data-slide-to="2"></li>
+  </ol>
+  <!--/.Indicators-->
+  <!--Slides-->
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <div class="view">
+        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg" alt="First slide">
+        <div class="mask rgba-black-light"></div>
       </div>
-      <div style="height: 30px;"></div>
+      <div class="carousel-caption">
+        <h3 class="h3-responsive">Light mask</h3>
+        <p>First text</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <!--Mask color-->
+      <div class="view">
+        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).jpg" alt="Second slide">
+        <div class="mask rgba-black-strong"></div>
+      </div>
+      <div class="carousel-caption">
+        <h3 class="h3-responsive">Strong mask</h3>
+        <p>Secondary text</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <!--Mask color-->
+      <div class="view">
+        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).jpg" alt="Third slide">
+        <div class="mask rgba-black-slight"></div>
+      </div>
+      <div class="carousel-caption">
+        <h3 class="h3-responsive">Slight mask</h3>
+        <p>Third text</p>
+      </div>
     </div>
   </div>
-  <br><br><br><br><br><br><br><br><br><br>
+  <!--/.Slides-->
+  <!--Controls-->
+  <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  <!--/.Controls-->
+</div>
+<!--/.Carousel Wrapper-->
 </div>
 
-  <script src="../../../assets/js/blueimp-gallery.min.js"></script>
-  <script src="../../../assets/js/countDown.js"></script>
-  <script src="../../../assets/js/parallax-card.js"></script>
 
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127827042-3"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+  <div class="col">
+    <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="../assets/img/test.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
 
-    gtag('config', 'UA-127827042-3');
-  </script>
+  </div>
+
+
+
+  <div class="col-2">col</div>
+</div>
+<br>
+<div class="row">
+  <div class="col-2">col-8</div>
+  <div class="col-5"><div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+</div></div>
+
+  <div class="col"><div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div></div>
+  <div class="col-2">col-4</div>
+</div>
+<div class="row">
+  <div class="col-2">col-8</div>
+  <div class="col-5">col-4</div>
+  <div class="col">col-4</div>
+  <div class="col-2">col-4</div>
+</div>
+<div class="row">
+  <div class="col-2">col-8</div>
+  <div class="col-5">col-4</div>
+  <div class="col">col-4</div>
+  <div class="col-2">col-4</div>
+</div>
