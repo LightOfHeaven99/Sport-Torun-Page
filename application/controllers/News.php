340401772f28ddaf57b0b7870475c8f14e4e3f0b
Główcześ <glowczes@gmail.com>
2019-02-28 06:34:37 +0100

    4

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -8,7 +8,16 @@ class News extends CI_Controller
       $this->load->helper('form');
       $this->load->library('form_validation');
       $this->load->model('news_model');
+
+      $this->data['posts'] = $this->news_model->get_posts();
   }
+
+
+  public function index()
+  {
+
+  }
+
 
   public function add_news()
   {
