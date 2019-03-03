fb29b60aa642fac61c795ad0dacac5d0a8c896d7
Kacper Burzała <kacperburzala@gmail.com>
2019-03-03 02:35:35 +0100

    8

--- application/views/pages/news.php
+++ application/views/pages/news.php
@@ -7,6 +7,17 @@
       <h1>Aktualności</h1>
       <div class="space20"></div>
 
+      <script>
+        /* Open when someone clicks on the span element */
+        function openNav() {
+          document.getElementById("myNav").style.width = "100%";
+        }
+
+        /* Close when someone clicks on the "x" symbol inside the overlay */
+        function closeNav() {
+          document.getElementById("myNav").style.width = "0%";
+        }
+      </script>
 
       <?php
         $query = $this->db->query("SELECT * FROM news ORDER BY id DESC");
