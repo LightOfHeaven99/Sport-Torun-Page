233a23d40a25dee0d2b75fe64c4e124a44f9f783
Główcześ <glowczes@gmail.com>
2019-03-04 19:24:03 +0100

    poprawa stopki

--- application/views/pages/faq.php
+++ application/views/pages/faq.php
@@ -48,7 +48,8 @@
         <p>Proszę zwrócić się do administratora strony za pomocą formularzu kontaktowego.</p>
       </div>
     </div>
-<br><br><br>
+  </div>
+<br><br><br><br><br><br>
 <script>
 
 const items = document.querySelectorAll(".accordion a");
