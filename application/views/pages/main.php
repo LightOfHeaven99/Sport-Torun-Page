456dfd3c9911cd255ae02deb61dbd7e31acb20bf
Kacper Burzała <kacperburzala@gmail.com>
2019-03-07 18:18:14 +0100

    5

--- application/views/pages/main.php
+++ application/views/pages/main.php
@@ -3,8 +3,9 @@
     <div class="main-logo-bg">
       <div class="main-player-bg">
         <div class="main-ball-bg">
-
-
+          
+        <div class="space200";></div>
+        <div class="space200";></div>
         <div class="space200";></div>
         <div class="space200";></div>
         <div class="space200";></div>
