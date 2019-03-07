0090e6e84b5c3d4666c1c01340f54f959afff608
Kacper Burzała <kacperburzala@gmail.com>
2019-03-07 18:17:22 +0100

    4

--- application/views/pages/main.php
+++ application/views/pages/main.php
@@ -1,16 +1,13 @@
 <!-- Page Content -->
 <div class="main-bg">
     <div class="main-logo-bg">
-      <div class="main-player-bg" style="height: 120%;">
+      <div class="main-player-bg">
         <div class="main-ball-bg">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6">
-        <h1 class="mt-5"></h1>
+
 
         <div class="space200";></div>
         <div class="space200";></div>
-        <div class="space100";></div>
+        <div class="space200";></div>
       </div>
     </div>
   </div>
