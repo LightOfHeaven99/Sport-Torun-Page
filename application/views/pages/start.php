3cb003ea31e79840e5e39ea0af5f42badd9fa229
Główcześ <glowczes@gmail.com>
2019-03-08 02:31:43 +0100

    36

--- application/views/pages/start.php
+++ application/views/pages/start.php
@@ -123,9 +123,11 @@ and is wrapped around the whole page content, except for the footer in this exam
 	 <div style="height: 20px;"></div>
 	 <div class="row justify-content-center">
 			<div class="row" style="padding: 2rem;">
+				<div class="col-sm-4"><center><img src="../assets/downloads/Schemat Logo.png"></img></center></div>
 				<div class="col-sm-4"><center><h6 class="header">Drużyna Pierwsza</h6></center></div>
 				<div class="col-sm-4"><center><h6 class="header">:</h4></center></div>
 				<div class="col-sm-4"><center><h6 class="header">Drużyna Druga</h6></center></div>
+				<div class="col-sm-4"><center><img src="../assets/downloads/Schemat Logo.png"></img></center></div>
 			</div>
 	 </div>
 	</div>
