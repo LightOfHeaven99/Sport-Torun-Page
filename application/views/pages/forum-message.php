84fca9f70c1a6daaa93cdb3195d31a9cc258d62f
Główcześ <glowczes@gmail.com>
2019-03-08 03:11:25 +0100

    Nowe kategorie

---
+++ application/views/pages/forum-message.php
@@ -0,0 +1,18 @@
+<!-- Page Content -->
+<div class="forum-bg">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <div class="space20"></div>
+        <div class="parallax-card-forum">
+          <br><br>
+          <h1 style="color: white; font-size: 30px;">Napisz wiadomość</h1>
+
+        </div>
+        <div class="space50"></div>
+      </div>
+    </div>
+  </div>
+
+
+</div>
---
+++ application/views/pages/forum-others.php
@@ -0,0 +1,18 @@
+<!-- Page Content -->
+<div class="forum-bg">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <div class="space20"></div>
+        <div class="parallax-card-forum">
+          <br><br>
+          <h1 style="color: white; font-size: 30px;"><font style="color: #364FD2;"><a href="forum-squads">Składy</a> / <a href="forum-stuff">Towar</a> / </font><b>Inne</b></h1>
+
+        </div>
+        <div class="space50"></div>
+      </div>
+    </div>
+  </div>
+
+
+</div>
