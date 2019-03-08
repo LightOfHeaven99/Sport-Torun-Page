a1172e555c4e8cfd99b48150dee23dc29534c36b
Główcześ <glowczes@gmail.com>
2019-03-08 02:36:05 +0100

    39

--- application/views/pages/start.php
+++ application/views/pages/start.php
@@ -122,7 +122,7 @@ and is wrapped around the whole page content, except for the footer in this exam
 	 </div>
 	 <div style="height: 20px;"></div>
 	 <div class="row justify-content-center">
-			<div class="row" style="padding: 2rem;">
+			<div class="row" style="padding: 5rem;">
 				<div class="col-sm-4"><center><h6 class="header">Drużyna Pierwsza</h6></center></div>
 				<div class="col-sm-4"><center><h6 class="header">:</h4></center></div>
 				<div class="col-sm-4"><center><h6 class="header">Drużyna Druga</h6></center></div>
