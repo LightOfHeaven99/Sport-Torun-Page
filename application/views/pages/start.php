3caa24228f8810c2024373669c4798865f9d04c5
Główcześ <glowczes@gmail.com>
2019-03-08 12:05:48 +0100

    3

--- application/views/pages/start.php
+++ application/views/pages/start.php
@@ -151,9 +151,8 @@ and is wrapped around the whole page content, except for the footer in this exam
           <br>
           <p><strong>Serdecznie zapraszamy</strong> wszystkich chętnych do zapisania się do Toruńskiej Ligi Siatkówki! Dostępny jest Formularz Zgłoszeniowy w zakładce "<a href="join">Zgłoszenie</a>", gdzie można wypełnić i przesłać potrzebne dokumenty, aby zgłosić się do nadchodzących rozgrywek. W razie jakichkolwiek pytań, proszę kontaktować się ze mną za pomocą Formularza Kontaktowego, który znajduje się w stopce strony.</p>
           <p class="w3-clear"></p>
-					<div class="w3-col m4 w3-hide-small">
-          <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-tag">0</span></span></p>
-        </div>
+					<p class="w3-right"><button class="w3-button w3-indigo" onclick="myFunction('demo1')" id="myBtn"><b>Komentarze  </b> <span class="w3-tag w3-white">1</span></button></p>
+
           <div class="w3-row w3-margin-bottom" id="demo1" style="display:none">
           </div>
         </div>
