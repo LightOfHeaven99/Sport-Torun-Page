028723e604ecd67bfba864f92311ce44897ec384
Główcześ <glowczes@gmail.com>
2019-03-08 03:13:25 +0100

    2

--- application/views/pages/forum-stuff.php
+++ application/views/pages/forum-stuff.php
@@ -6,7 +6,7 @@
         <div class="space20"></div>
         <div class="parallax-card-forum">
           <br><br>
-          <h1 style="color: white; font-size: 30px;"><font style="color: #364FD2;"><a href="forum-squads">Szukam zawodników</a> / </font><b>Szukam drużyny</b><font style="color: #364FD2;"> / <a href="forum-others">Inne</a></font></h1>
+          <h1 style="color: white; font-size: 30px;"><font style="color: #364FD2;"><a href="forum-squads">Składy</a> / </font><b>Towar</b><font style="color: #364FD2;"> / <a href="forum-others">Inne</a></font></h1>
 
         </div>
         <div class="space50"></div>
