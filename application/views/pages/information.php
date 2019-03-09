9ec496a1e6b57b5a640165cd3caa3f063edc8faf
Kacper Burzała <kacperburzala@gmail.com>
2019-03-09 10:25:49 +0100

    8

--- application/views/pages/information.php
+++ application/views/pages/information.php
@@ -1,20 +1,25 @@
 <!-- Page Content -->
-<div class="information-bg">
+<div class="timetable-bg">
   <div class="container">
     <div class="row">
       <div class="col-lg-12 text-center">
         <div class="space20"></div>
-        <div class="parallax-card-timetable">
-          <br><br><br><br><br><br><br>
-          <h3 style="color: white; font-size: 20px;">__________________________</h3>
-          <br>
-          <h3 style="color: white; font-size: 20px;"><b>Informacje są właśnie przygotowywane!</b></h3>
-          <h3 style="color: white; font-size: 20px;">__________________________</h3>
+        <div class="card" style="width: auto;">
+          <div class="card-header">
+          <br><br>
+          <br><br>
+
+          <p style="color: white;">Informacje o lidze zostaną uzupełnione wraz z pojawianiem się pierwszych drużyn.</p>
+
+          <br><br>
+          <br><br>
+        </div>
         </div>
         <div class="space50"></div>
       </div>
     </div>
   </div>
+</div>
 
 
 </div>
--- application/views/pages/players.php
+++ application/views/pages/players.php
@@ -1,20 +1,25 @@
 <!-- Page Content -->
-<div class="players-bg">
+<div class="timetable-bg">
   <div class="container">
     <div class="row">
       <div class="col-lg-12 text-center">
         <div class="space20"></div>
         <div class="card" style="width: auto;">
-          <br><br><br><br><br><br><br>
-          <h3 style="color: white; font-size: 20px;">__________________________</h3>
-          <br>
-          <h3 style="color: white; font-size: 20px;"><b>Zawodnicy są właśnie przygotowywani!</b></h3>
-          <h3 style="color: white; font-size: 20px;">__________________________</h3>
+          <div class="card-header">
+          <br><br>
+          <br><br>
+
+          <p style="color: white;">W bazie nie ma jeszcze żadnych zawodników.</p>
+
+          <br><br>
+          <br><br>
+        </div>
         </div>
         <div class="space50"></div>
       </div>
     </div>
   </div>
+</div>
 
 
 </div>
