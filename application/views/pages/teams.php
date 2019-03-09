bb9d7c8516efa23bea29ffaaaa1e58a5828336b9
Główcześ <glowczes@gmail.com>
2019-03-09 02:17:10 +0100

    teams2

--- application/views/pages/teams.php
+++ application/views/pages/teams.php
@@ -7,7 +7,7 @@
         <div class="card" style="width: auto;">
           <div class="card-header">
           <br><br>
-          <img src="../assets/img/facebook-logo2.png" alt="logo">
+          <img src="../assets/img/facebook-logo2.png" alt="logo" height="40px" width="40px">
           <p style="color: white; font-size: 20px;">Drużyna 1</p>
         </div>
         <div class="space50"></div>
