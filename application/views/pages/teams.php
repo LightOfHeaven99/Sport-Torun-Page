255f2ac85a7b6327b0d66ed6b36e8626d4a9e0fe
Główcześ <glowczes@gmail.com>
2019-03-09 02:12:41 +0100

    drużyny

--- application/views/pages/teams.php
+++ application/views/pages/teams.php
@@ -1,20 +1,20 @@
 <!-- Page Content -->
-<div class="teams-bg">
+<div class="timetable-bg">
   <div class="container">
     <div class="row">
       <div class="col-lg-12 text-center">
         <div class="space20"></div>
-        <div class="parallax-card-timetable">
-          <br><br><br><br><br><br><br>
-          <h3 style="color: white; font-size: 20px;">__________________________</h3>
-          <br>
-          <h3 style="color: white; font-size: 20px;"><b>Drużyny są właśnie przygotowywane!</b></h3>
+        <div class="card" style="width: auto;">
+          <div class="card-header">
+          <br><br>
+          <img src="" alt="">
           <h3 style="color: white; font-size: 20px;">__________________________</h3>
         </div>
         <div class="space50"></div>
       </div>
     </div>
   </div>
+</div>
 
 
 </div>
