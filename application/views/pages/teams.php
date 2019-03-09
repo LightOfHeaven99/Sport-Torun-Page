903917a80591e5d26fd09453bbfef1a5328ddf65
Kacper Burzała <kacperburzala@gmail.com>
2019-03-09 22:43:23 +0100

    19

--- application/views/pages/teams.php
+++ application/views/pages/teams.php
@@ -15,40 +15,59 @@
 
           <div class="card-header">
             <div class="row">
-            &nbsp; &nbsp;
-            <?php echo '<img src = "data:image/jpeg;base64,'.base64_encode( $row->logo ).'" style="width: 70px; height: 70px;" class="responsive"/>'; ?>
-            &nbsp;&nbsp;&nbsp;<a class="team-name" href=""><?= $row->name;?></a>
+              <div class="col-sm-3">
+                &nbsp; &nbsp;
+                <?php echo '<img src = "data:image/jpeg;base64,'.base64_encode( $row->logo ).'" style="width: 70px; height: 70px;" class="responsive"/>'; ?>
+                &nbsp;&nbsp;&nbsp;<a class="team-name" href="" onclick=""><?= $row->name;?></a>
+              </div>
+
+              <!-- on click-->
+              <div class="col-sm-9" style="text-align: center; margin-left: auto; margin-right: auto;">
             <?php
               $query_players = $this->db->query("SELECT * FROM players");
 
               foreach($query_players->result() as $row_players):
                 //if($row_players->id != 0) :
-                  if($row_players->id == $row->player1_id) :
-                    echo '<img src = "data:image/jpeg;base64,'.base64_encode( $row_players->photo ).'" style="width: 100px; height: 100px;" class="responsive"/>';
-                    ?> <br> <?php
-                    echo $row_players->first_name;
-                    ?> &nbsp; <?php
-                    echo $row_players->last_name;
-                  endif;
 
-                  if($row_players->id == $row->player2_id) :
-                    echo '<img src = "data:image/jpeg;base64,'.base64_encode( $row_players->photo ).'" style="width: 100px; height: 100px;" class="responsive"/>';
-                    ?> <br> <?php
-                    echo $row_players->first_name;
-                    ?> &nbsp; <?php
-                    echo $row_players->last_name;
-                  endif;
+                  function show_player_after_click($current_id) {
+                    if($row_players->id == $current_id) :
+                      echo '<img src = "data:image/jpeg;base64,'.base64_encode( $row_players->photo ).'" style="width: 100px; height: 100px;" class="responsive"/>';
+                      echo '<br>';
+                      echo $row_players->first_name;
+                      echo '&nbsp;';
+                      echo $row_players->last_name;
+                    endif;
+                  }
 
-                  if($row_players->id == $row->player3_id) :
-                    echo '<img src = "data:image/jpeg;base64,'.base64_encode( $row_players->photo ).'" style="width: 100px; height: 100px;" class="responsive"/>';
-                    ?> <br> <?php
-                    echo $row_players->first_name;
-                    ?> &nbsp; <?php
-                    echo $row_players->last_name;
-                  endif;
+                  echo '<div class="row"';
+                    show_player_after_click($row->player1_id);
+                    show_player_after_click($row->player2_id);
+                    show_player_after_click($row->player3_id);
+                    show_player_after_click($row->player4_id);
+                  echo '</div>';
+                  echo '<br>';
+                  echo '<div class="row"';
+                    show_player_after_click($row->player5_id);
+                    show_player_after_click($row->player6_id);
+                    show_player_after_click($row->player7_id);
+                    show_player_after_click($row->player8_id);
+                  echo '</div>';
+                  echo '<br>';
+                  echo '<div class="row"';
+                    show_player_after_click($row->player9_id);
+                    show_player_after_click($row->player10_id);
+                    show_player_after_click($row->player11_id);
+                    show_player_after_click($row->player12_id);
+                  echo '</div>';
+                  echo '<br>';
+                  echo '<div class="row"';
+                    show_player_after_click($row->player13_id);
+                    show_player_after_click($row->player14_id);
+                    show_player_after_click($row->player15_id);
+                  echo '</div>';
                 //endif;
             ?>
-
+          </div>
             <?php endforeach; ?>
             </div>
           </div>
