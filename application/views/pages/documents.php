f7edd22256f44f1b525251f7a2c98dda89d582bb
Główcześ <glowczes@gmail.com>
2019-03-10 18:07:15 +0100

    poprawka

--- application/views/pages/documents.php
+++ application/views/pages/documents.php
@@ -17,7 +17,6 @@
           <a href="../assets/downloads/Polityka Cookies.pdf" style="color: #364FD2; font-size: 20px;">Polityka cookies</a><br>
           <h3 style="color: white; font-size: 20px;">__________________________</h3>
           <a href="../assets/downloads/Polityka Prywatności.pdf" style="color: #364FD2; font-size: 20px;">Polityka prywatności</a>
-          <h3 style="color: white; font-size: 20px;">__________________________</h3>
         </div>
         <div class="space50"></div>
       </div>
