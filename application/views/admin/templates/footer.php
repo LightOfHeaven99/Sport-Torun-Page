f344a17418de0dab0c39ce3a36c4f1c42c1775e1
Kacper Burzała <kacperburzala@gmail.com>
2019-03-13 22:12:13 +0100

    10

--- application/views/admin/templates/footer.php
+++ application/views/admin/templates/footer.php
@@ -1,8 +1,8 @@
 <!-- Footer -->
 <footer class="py-1 bg-dark">
-  <br><br>
+  <br>
     <center><p class="text-white"><font size="3">Panel Admina | Wsparcie techniczne: +48 530 543 770</font></p></center>
-  <br><br>
+  <br>
 </footer>
 
 </body>
